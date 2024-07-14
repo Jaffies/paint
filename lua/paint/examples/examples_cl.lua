c75a0c7a5b880eea8d60058350056b1a0863f5a0
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2024-07-14 10:17:52 -0500

    Added fonts, formatting functions, and documented examples class

--- lua/paint/examples/examples_cl.lua
+++ lua/paint/examples/examples_cl.lua
@@ -1,8 +1,140 @@
-local examples = {}
-local paint = paint
+---@class paint.examples
+---@field title fun(parent:Panel, text:string) : nil
+---@field header fun(parent:Panel, text:string) : nil
+---@field subheader fun(parent:Panel, text:string) : nil
+---@field text fun(parent:Panel, text:string) : nil
+---@field boldText fun(parent:Panel, text:string) : nil
+---@field footer fun(parent:Panel, height:number) : nil
+---@field create fun() : nil
+---@field controls table<string, {name:string, func:fun():Panel, icon:Material}> # Controls to be added to the examples menu
+---@field addControl fun(name:string, func:fun():Panel, icon:Material) : nil
 
---- NO clean code here
---- Since it's made only for example
+local examples = {
+	controls = {}
+}
+paint.examples = examples
+
+local COLOR_BACKGROUND = Color( 148, 152, 156 )
+
+--#region Fonts
+
+local titleFont = "paint_examples_title"
+surface.CreateFont( titleFont, {
+	font = "Roboto",
+	size = 32,
+	weight = 500,
+	antialias = true
+} )
+
+local headerFont = "paint_examples_header"
+surface.CreateFont( headerFont, {
+	font = "Roboto",
+	size = 24,
+	weight = 500,
+	antialias = true
+} )
+
+local subheaderFont = "paint_examples_subheader"
+surface.CreateFont( subheaderFont, {
+	font = "Roboto",
+	size = 20,
+	weight = 500,
+	antialias = true
+} )
+
+local boldTextFont = "paint_examples_bold_text"
+surface.CreateFont( boldTextFont, {
+	font = "Roboto",
+	size = 16,
+	weight = 600,
+	antialias = true
+} )
+
+local textFont = "paint_examples_text"
+surface.CreateFont( textFont, {
+	font = "Roboto",
+	size = 16,
+	weight = 500,
+	antialias = true
+} )
+
+--#endregion Fonts
+
+--#region Example Formatting Functions
+
+--- Adds a title to the parent panel
+---@param parent Panel
+---@param text string
+function examples.title( parent, text )
+	local title = parent:Add( "DLabel" )
+	title:SetFont( titleFont )
+	title:SetText( text )
+	title:SizeToContents()
+	title:Dock( TOP )
+	title:SetContentAlignment( 4 )
+	title:SetColor( color_black )
+	title:DockMargin( 15, 15, 15, 0 )
+end
+
+--- Adds a header to the parent panel
+---@param parent Panel
+---@param text string
+function examples.header( parent, text )
+	local header = parent:Add( "DLabel" )
+	header:SetFont( headerFont )
+	header:SetText( text )
+	header:SizeToContents()
+	header:Dock( TOP )
+	header:SetContentAlignment( 4 )
+	header:SetColor( color_black )
+	header:DockMargin( 15, 15, 15, 5 )
+end
+
+--- Adds a subheader to the parent panel
+---@param parent Panel
+---@param text string
+function examples.subheader( parent, text )
+	local header = parent:Add( "DLabel" )
+	header:SetFont( subheaderFont )
+	header:SetText( text )
+	header:SizeToContents()
+	header:Dock( TOP )
+	header:SetContentAlignment( 4 )
+	header:SetColor( color_black )
+	header:DockMargin( 15, 5, 15, 5 )
+end
+
+--- Adds a bold text label to the parent panel
+---@param parent Panel
+---@param text string
+function examples.boldText( parent, text )
+	local label = parent:Add( "DLabel" )
+	label:SetFont( boldTextFont )
+	label:SetAutoStretchVertical( true )
+	label:Dock( TOP )
+	label:SetColor( color_black )
+	label:SetText( text )
+	label:SetWrap( true )
+	label:DockMargin( 15, 5, 15, 0 )
+end
+
+--- Adds a text label to the parent panel
+---@param parent Panel
+---@param text string
+function examples.text( parent, text )
+	local label = parent:Add( "DLabel" )
+	label:SetFont( textFont )
+	label:SetAutoStretchVertical( true )
+	label:Dock( TOP )
+	label:SetColor( color_black )
+	label:SetText( text )
+	label:SetWrap( true )
+	label:DockMargin( 15, 5, 15, 0 )
+end
+
+--#endregion Example Formatting Functions
+
+
 function examples.create()
 	local frame = vgui.Create('DFrame')
 
@@ -17,9 +149,11 @@ function examples.create()
 	for k, v in pairs(examples.controls) do
 		propertySheet:AddSheet(v.name, v.func(), v.icon)
 	end
+
+	frame:MakePopup()
 end
 
-examples.controls = {}
+
 function examples.addControl(name, func, icon)
 	examples.controls[name] = {name = name, func = func, icon = icon}
 end
