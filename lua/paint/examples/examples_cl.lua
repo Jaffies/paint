2824f33f5efa3376447fe139aeeb43c68f8be54e
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2024-07-14 10:15:41 -0500

    Moved example loading to examples_cl to make hotloading work better for development

--- lua/paint/examples/examples_cl.lua
+++ lua/paint/examples/examples_cl.lua
@@ -24,4 +24,22 @@ function examples.addControl(name, func, icon)
 	examples.controls[name] = {name = name, func = func, icon = icon}
 end
 
-paint.examples = examples
+--#region Load Examples
+
+local function load(path)
+	AddCSLuaFile(path)
+	if CLIENT then
+		include(path)
+	end
+end
+
+load('paint/examples/vgui/markup_richtext_cl.lua')
+load('paint/examples/controls/lines_cl.lua')
+load('paint/examples/controls/rects_cl.lua')
+load('paint/examples/controls/rounded_boxes_cl.lua')
+load('paint/examples/controls/outlines_cl.lua')
+load('paint/examples/controls/batch_cl.lua')
+load('paint/examples/controls/blur_cl.lua')
+load('paint/examples/controls/main_cl.lua')
+
+--#endregion Load Examples
