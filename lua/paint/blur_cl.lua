2a6ea97bcd2b187424ad62c1994e36eae89161ba
Your Name <you@example.com>
2024-07-26 01:36:11 -0400

    Fix blur_cl.lua

--- lua/paint/blur_cl.lua
+++ lua/paint/blur_cl.lua
@@ -7,11 +7,11 @@ local paint = paint
 	It doesn't ocupy smalltex1 now. Use it freely)
 ]]
 
-local convarBlur = CreateConVar('paint_blur', 10, FCVAR_ARCHIVE, 'Amount of blur that needs to apply', 0, 100)
-local convarBlurPasses = CreateConVar('paint_blur_passes', 2, FCVAR_ARCHIVE, 'Amount of blur passes that needs to apply', 0, 30)
-local convarBlurFPS = CreateConVar('paint_blur_fps', 20, FCVAR_ARCHIVE, 'How many FPS needed for blur?', 1, 60)
+local convarBlur = CreateConVar('paint_blur', '10', FCVAR_ARCHIVE, 'Amount of blur that needs to apply', 0, 100)
+local convarBlurPasses = CreateConVar('paint_blur_passes', '2', FCVAR_ARCHIVE, 'Amount of blur passes that needs to apply', 0, 30)
+local convarBlurFPS = CreateConVar('paint_blur_fps', '20', FCVAR_ARCHIVE, 'How many FPS needed for blur?', 1, 60)
 
-local texture = GetRenderTargetEx('paint_blur_rt', 256, 256, RT_SIZE_DEFAULT, MATERIAL_RT_DEPTH_NONE, bit.band(2, 256, 32768), 0, IMAGE_FORMAT_RGB888)
+local texture = GetRenderTargetEx('paint_blur_rt', 256, 256, RT_SIZE_DEFAULT, MATERIAL_RT_DEPTH_NONE, bit.band(2, 256), 0, IMAGE_FORMAT_RGB888)
 
 do
 	local getInt = FindMetaTable('ConVar').GetInt
@@ -38,8 +38,8 @@ do
 			setColorMaterial()
 			drawScreenQuad()
 
-			overrideAlphaWriteEnable(false)
-			overrideColorWriteEnable(false)
+			overrideAlphaWriteEnable(false, false)
+			overrideColorWriteEnable(false, false)
 		popRenderTarget()
 		-- Even if this RT doesn't use alpha channel (IMAGE_FORMAT), it stil somehow uses alpha... BAD!
 		-- At least no clearDepth
@@ -103,8 +103,5 @@ do
 		return mat
 	end
 end
-
----@class paint
----@field blur blur
 
 paint.blur = blur
